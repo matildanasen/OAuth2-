2bb88c3091446a7a293ca5f94eb62327cdc39701
Matilda Näsén <matildanasen@Matildas-MBP.lan>
2020-04-22 20:01:56 +0200

    created auth for GitHub

---
+++ build/precache-manifest.d3db3b80f2d2fa3294667f89bff9acd5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ba765ff95afef4a8355c574214bc3155",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7642a09e83c39e3e09c0",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "9578308243ff0983ac27",
+    "url": "/static/js/2.a189f102.chunk.js"
+  },
+  {
+    "revision": "e120dc181622ca4ee8c2c0852b689a62",
+    "url": "/static/js/2.a189f102.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7642a09e83c39e3e09c0",
+    "url": "/static/js/main.80b20c07.chunk.js"
+  },
+  {
+    "revision": "0f7e17235ce2106ccee1",
+    "url": "/static/js/runtime-main.549be430.js"
+  }
+]);
--- src/App.js
+++ src/App.js
@@ -1,26 +1,54 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import firebase from "firebase"
+import StyledFirebaseAuth from "react-firebaseui/StyledFirebaseAuth"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+firebase.initializeApp({
+  apiKey:"AIzaSyC14P32yiAlGXQc-uVA-jh4LE-6IJrKp-8",
+  authDomain:"oauth3-986c0.firebaseapp.com"
+  
+})
+
+class App extends Component {
+state = {isSignedIn: false}
+  uiConfig = {
+    signInFlow: "popup", 
+    signInOptions: [
+      firebase.auth.GithubAuthProvider.PROVIDER_ID
+    ],
+    callbacks: {
+      signInSucess: () => false
+    }
+  }
+
+  componentDidMount = ()=> {
+    firebase.auth().onAuthStateChanged(user => {
+      this.setState({isSignedIn: !!user})
+    })
+  }
+  
+  render() {
+    return (
+      <div className="App">
+        {this.state.isSignedIn ? (
+          <span>
+            <div>Signed In!</div>
+            <button onClick={() => firebase.auth().signOut()}>Sign out!</button>
+            <h1>Welcome {firebase.auth().currentUser.displayName}</h1>
+            <img
+              alt="profile picture"
+              src={firebase.auth().currentUser.photoURL}
+            />
+          </span>
+        ) : (
+          <StyledFirebaseAuth
+            uiConfig={this.uiConfig}
+            firebaseAuth={firebase.auth()}
+          />
+        )}
+      </div>
+    )
+  }
 }
 
-export default App;
+export default App
