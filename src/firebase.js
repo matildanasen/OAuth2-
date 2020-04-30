e04d6792c71b03b05b5887bf7f25da7b2ae11af7
Matilda Näsén <50735503+matildanasen@users.noreply.github.com>
2020-04-30 14:53:54 +0200

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -4,13 +4,13 @@ import "firebase/firestore";
 
 // Web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyC14P32yiAlGXQc-uVA-jh4LE-6IJrKp-8",
+  apiKey: "",
   authDomain: "oauth3-986c0.firebaseapp.com",
   databaseURL: "https://oauth3-986c0.firebaseio.com",
-  projectId: "oauth3-986c0",
+  projectId: "",
   storageBucket: "oauth3-986c0.appspot.com",
-  messagingSenderId: "247680283452",
-  appId: "1:247680283452:web:a5333ff1bf1b3e164be439",
+  messagingSenderId: "",
+  appId: "",
 };
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
